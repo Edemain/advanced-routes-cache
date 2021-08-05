e7ade0221d8098dc7e145e21fda6574aec29f218
lixinglu <xinglu.li@1905.com>
2021-08-05 20:41:45 +0800

    feat(element-plus): update element-plus

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,6 @@
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
-
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
